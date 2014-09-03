7ec544331184a8f2597237be50e0734558ffa550
mhelvens <mhelvens@gmail.com>
2014-09-03 15:21:22 +0200

    Fixed a mistake in the gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,48 @@ var gulp = require('gulp'),
     rimraf = require('rimraf');
 
 gulp.task('traceur', function (callback) {
-	gulp.src('src/**/*.js')
-		  .pipe(traceur({
-			  script: true,
-			  sourceMaps: true
-		  }))
-		  .pipe(gulp.dest('tmp-build'))
-		  .on('end', callback);
+    gulp.src('src/**/*.js')
+        .pipe(traceur({
+            script: true,
+            sourceMaps: true
+        }))
+        .pipe(gulp.dest('tmp-build'))
+        .pipe(gulp.dest('dist')) // temporary?
+        .on('end', callback);
 });
 
 gulp.task('webpack', ['traceur'], function (callback) {
-	webpack({
-		entry: './tmp-build/amy-circuitboard.js',
-		output: {
-			path: './dist',
-			filename: 'amy-core.js',
-			libraryTarget: 'amd'
-		},
-		externals: ['jquery', 'jquery-ui']
-	}, function (err, stats) {
-		if (err) { throw new gutil.PluginError('webpack', err) }
-		gutil.log(stats.toString({ colors: true }));
-		callback();
-	})
+    webpack({
+        entry: './tmp-build/amy-circuitboard.js',
+        output: {
+            path: './dist',
+            filename: 'amy-core.js',
+            libraryTarget: 'amd'
+        },
+        externals: ['jquery', 'jquery-ui']
+    }, function (err, stats) {
+        if (err) {
+            throw new gutil.PluginError('webpack', err)
+        }
+        gutil.log(stats.toString({ colors: true }));
+        callback();
+    })
 });
 
 gulp.task('uglify', ['webpack'], function (callback) {
-	gulp.src('./dist/amy-core.js')
-		  .pipe(uglify())
-		  .pipe(rename({suffix: '.min'}))
-		  .on('end', callback);
+    gulp.src('./dist/amy-core.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'))
+        .on('end', callback);
 });
 
 gulp.task('clean-tmp', ['traceur', 'webpack'], function (callback) {
-	rimraf('tmp-build', callback);
+    rimraf('tmp-build', callback);
 });
 
 gulp.task('build', ['traceur', 'webpack', 'uglify', 'clean-tmp']);
 
 gulp.task('watch', function () {
-	gulp.watch(['**/*.js', '!bower_components/**/*.*', '!node_modules/**/*.*'], ['traceur', 'webpack']);
+    gulp.watch(['**/*.js', '!bower_components/**/*.*', '!node_modules/**/*.*', '!gulpfile.js'], ['traceur', 'webpack']);
 });
