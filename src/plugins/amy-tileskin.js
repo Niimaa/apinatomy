97b17b918078b3f4886ad9afd4866e35f5a5af05
mhelvens <mhelvens@gmail.com>
2014-09-04 22:51:31 +0200

    Allowing css styling per tile specified in model, enacted by the tileskin plugin.

--- src/plugins/amy-tileskin.js
+++ src/plugins/amy-tileskin.js
@@ -1,33 +1,39 @@
 define(['jquery', '../util.js'], function ($) {
 
-    $.CSS('.skinned-tile')
-        .addRule('display', 'flex')
-        .addRule('flex-direction', 'column');
-    $.CSS('.skinned-tile > header')
-        .addRule('display', 'flex')
-        .addRule('align-items', 'center')
-        .addRule('justify-content', 'center')
-        .addRule('font-weight', 'bold')
-        .addRule('border-width', '1px')
-        .addRule('overflow', 'hidden');
-    $.CSS('.skinned-tile.open > header')
-        .addRule('height', '26px')
-        .addRule('border-style', 'none none solid none')
-        .addRule('line-height', '26px')
-        .addRule('font-size', '19.5px')
-        .addRule('white-space', 'nowrap');
-    $.CSS('.skinned-tile:not(.open) > header')
-        .addRule('flex-grow', 1);
-    $.CSS('.skinned-tile.open > section')
-        .addRule('flex-grow', 1);
-    $.CSS('.skinned-tile:not(.open) > section')
-        .addRule('display', 'none');
+	$.CSS('.skinned-tile')
+		  .addRule('display', 'flex')
+		  .addRule('user-select', 'none')
+		  .addRule('flex-direction', 'column');
 
-    return function skin(tile) {
-        var origElement = tile.html;
-        origElement.addClass('skinned-tile');
-        origElement.append(`<header>${tile.model.id}</header><section/>`);
-        tile.html = origElement.children('section');
-        tile.html.css('padding', tile.options.tileSpacing)
-    }
+	$.CSS('.skinned-tile > header')
+		  .addRule('display', 'flex')
+		  .addRule('align-items', 'center')
+		  .addRule('justify-content', 'center')
+		  .addRule('font-weight', 'bold')
+		  .addRule('border-width', '1px')
+		  .addRule('overflow', 'hidden');
+	$.CSS('.skinned-tile.open > header')
+		  .addRule('height', '26px')
+		  .addRule('border-style', 'none none solid none')
+		  .addRule('line-height', '26px')
+		  .addRule('font-size', '19.5px')
+		  .addRule('white-space', 'nowrap');
+	$.CSS('.skinned-tile:not(.open) > header')
+		  .addRule('flex-grow', 1);
+
+	$.CSS('.skinned-tile > section')
+		  .addRule('user-select', 'text');
+	$.CSS('.skinned-tile.open > section')
+		  .addRule('flex-grow', 1);
+	$.CSS('.skinned-tile:not(.open) > section')
+		  .addRule('display', 'none');
+
+	return function skin(tile) {
+		var origElement = tile.html;
+		origElement.addClass('skinned-tile');
+		origElement.append(`<header>${tile.model.id}</header><section/>`);
+		tile.html = origElement.children('section');
+		tile.html.css('padding', tile.options.tileSpacing);
+		tile.element.css(tile.model.css);
+	}
 });
