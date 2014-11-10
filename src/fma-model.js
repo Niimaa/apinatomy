6aee430650f8333a6d36ca7467dd6b003a1f0de6
mhelvens <mhelvens@gmail.com>
2014-11-10 16:05:07 +0100

    a slightly improved and better documented fma-model retriever

--- src/fma-model.js
+++ src/fma-model.js
@@ -1,30 +1,43 @@
 define(['jquery', 'bluebird', './util/defer.js'], function ($, P, defer) {
 	'use strict';
 
-	var _deferredCache = {};
-	function _getDeferred(id) {
-		if (!_deferredCache[id]) { _deferredCache[id] = defer() }
-		return _deferredCache[id];
-	}
 
+	// This module implements an interface to the FMA database on the old prototype
+	// server, implementing the interface expected by ApiNATOMY. It creates a linked
+	// object structure that preserves the original DAG structure. It does so by
+	// maintaining a cache that maps each id to its corresponding object.
+	//
+	// The implementation of this module assumes that the database returns models
+	// with the following structure:
+	//
+	// ModelType = {
+	//     _id: string
+	//     sub: [{  entity: { _id: string }  }]
+	// }
+
+
+	/* the prototype for model objects, implementing the expected interface */
 	var _modelPrototype = {
 		getChildIds()  { return this.sub.map((sub) => sub.entity._id) },
 		getModels(ids) { return getFmaModels(ids) },
 		get id() { return this._id }
 	};
 
-	function removeNullChildren(model) {
-		for (var i = model.sub.length - 1; i >= 0; --i) {
-			if (model.sub[i].entity === null) {
-				model.sub.splice(i);
-			}
-		}
-	}
 
+	/* storing and retrieving 'deferreds' to models */
+	var _getDeferred = (() => {
+		var _deferredCache = {};
+		return (id) => {
+			if (!_deferredCache[id]) { _deferredCache[id] = defer() }
+			return _deferredCache[id];
+		};
+	})();
+
+
+	/* to retrieve an array of promises to models, given an array of ids */
 	function getFmaModels(ids) {
-		//
-		// filter out the ids that we have not requested from the server before
-		//
+
+		/* gather the ids that we have not requested from the server before */
 		var newIds = [];
 		ids.forEach((id) => {
 			if (!_getDeferred(id).alreadyRequested) {
@@ -33,26 +46,36 @@ define(['jquery', 'bluebird', './util/defer.js'], function ($, P, defer) {
 			}
 		});
 
-		//
-		// request and build the model objects belonging to those ids
-		//
+		/* request and build the model objects belonging to those ids */
 		P.resolve($.ajax({
 			url: `http://www.apinatomy.org:8766/resources/entities/${newIds.join(',')}`,
 			dataType: 'jsonp'
-		})).then((response) => {
-			response.forEach((modelObj) => {
-				var newModel = Object.create(_modelPrototype);
-				$.extend(newModel, modelObj);
-				removeNullChildren(newModel);
-				_getDeferred(newModel._id).resolve(newModel);
-			});
+		})).each((modelObj) => {
+
+			/*  remove references to children that are not actually   */
+			/*  in the database (the FMA database is messy that way)  */
+			for (var i = modelObj.sub.length - 1; i >= 0; i -= 1) {
+				if (modelObj.sub[i].entity === null) {
+					modelObj.sub.splice(i);
+				}
+			}
+
+			/* create the new model object based on the prototype */
+			var newModel = Object.create(_modelPrototype);
+
+			/* assign the retrieved model values to the new model object */
+			$.extend(newModel, modelObj);
+
+			/* resolve the corresponding promise */
+			_getDeferred(newModel._id).resolve(newModel);
+
 		});
 
-		//
-		// return an array of promises to all requested ids
-		//
+		/* return an array of promises to all requested ids */
 		return ids.map((id) => _getDeferred(id).promise);
+
 	}
 
 	return getFmaModels;
+
 });
