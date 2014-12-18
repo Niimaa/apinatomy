4bdc07aa2b9915e4322d2c587b2ed780babb4298
mhelvens <mhelvens@gmail.com>
2014-12-18 20:04:37 +0100

    structured through new generator-mhelvens-library 0.6.0

--- src/example/example.js
+++ src/example/example.js
@@ -1,105 +1,72 @@
-/*  RequireJS Configuration */
-requirejs.config({
-	paths: {
-		'domReady':  '../../bower_components/requirejs-domready/domReady',
-		'jquery':    '../../bower_components/jquery/dist/jquery',
-		'js-graph':  '../../bower_components/js-graph/dist/js-graph',
-		'delta-js':  '../../bower_components/delta.js/dist/delta',
-		'bluebird':  '../../bower_components/bluebird/js/browser/bluebird',
-		'chroma-js': '../../bower_components/chroma-js/chroma',
-		'd3':        '../../bower_components/d3/d3',
-		'three-js':  '../../bower_components/three.js/three',
-		'baconjs':   '../../bower_components/bacon/dist/Bacon.js'
-	},
-	shim: {
-		'jquery':   { exports: 'jQuery' },
-		'bluebird': { init() { this.longStackTraces() } },
-		'three-js': { exports: 'THREE' }
-	}
-});
-
-
-/* make 'jQuery' globally available for Bacon.js to find */
-window.jQuery = require('jquery');
-
-
-/* load the circuitboard code and style */
-require('../circuitboard.js');
+/* styling */
 require('./example.scss');
 
+/* libraries */
+var $ = require('expose?jQuery!jquery');
 
-/* example application  (circuitboard.js has to be loaded first) */
-require([
-
-	/* libraries that return a variable */
-	'jquery',
-	'../fma-model.js',
-
-	/* plugins */
-	'../p-core.js',
-	'../p-tile-skin.js',
-	'../p-tile-spacing.js',
-	'../p-tile-click-to-open.js',
-	'../p-tile-weight.js',
-	'../p-tile-active.js',
-	'../p-tile-open.js',
-	'../p-tile-grow-when-open.js',
-	'../p-tile-open-active.js',
-	'../p-tile-skin-grow-when-open.js',
-	'../p-position-tracking.js',
-	'../p-transition-position-tracking.js',
-	'../p-tile-hidden.js',
-	'../p-tile-maximized.js',
-	'../p-tile-middleclick-to-maximize.js',
-	'../p-d3.js',
-	'../p-animation-loop.js', // TODO: figure out why we get an error when this is moved down one line
-	'../p-ppi.js',
-	'../p-three-d.js',
-	'../p-three-d-geometric-models.js',
-	'../p-three-d-geometric-models-stl.js',
-	'../p-d3-three-d.js',
-	'../p-three-d-manual-controls.js',
-	'../p-three-d-auto-controls.js'
-
-], function ($, getFmaModels) {
-	'use strict';
+/* load the circuitboard, model loader and plugins */
+var circuitboard = require('../circuitboard.js');
+var getFmaModels = require('../fma-model.js');
+require('../p-core.js');
+require('../p-tile-skin.js');
+require('../p-tile-spacing.js');
+require('../p-tile-click-to-open.js');
+require('../p-tile-weight.js');
+require('../p-tile-active.js');
+require('../p-tile-open.js');
+require('../p-tile-grow-when-open.js');
+require('../p-tile-open-active.js');
+require('../p-tile-skin-grow-when-open.js');
+require('../p-position-tracking.js');
+require('../p-transition-position-tracking.js');
+require('../p-tile-hidden.js');
+require('../p-tile-maximized.js');
+require('../p-tile-middleclick-to-maximize.js');
+require('../p-d3.js');
+require('../p-animation-loop.js'); // TODO: check if we still get an error when this is moved down one line
+require('../p-ppi.js');
+require('../p-three-d.js');
+require('../p-three-d-geometric-models.js');
+require('../p-three-d-geometric-models-stl.js');
+require('../p-d3-three-d.js');
+require('../p-three-d-manual-controls.js');
+require('../p-three-d-auto-controls.js');
 
 
-	/* select plugins to activate them  (note that these must already be *loaded* at this point) */
-	$.circuitboard.plugin([
-		'tile-skin',
-		'tile-click-to-open',
-		'tile-grow-when-open',
-		'tile-middleclick-to-maximize',
-		'tile-spacing',
-		'tile-active',
-		//'ppi',
-		'three-d',
-		'three-d-manual-controls'
-		//'three-d-auto-controls',
-		//'three-d-geometric-models',
-		//'three-d-geometric-models-stl'
-	]);
+/* select plugins to activate them  (note that these must already be *loaded* at this point) */
+circuitboard.plugin([
+	'tile-skin',
+	'tile-click-to-open',
+	'tile-grow-when-open',
+	'tile-middleclick-to-maximize',
+	'tile-spacing',
+	'tile-active',
+	//'ppi',
+	'three-d',
+	'three-d-manual-controls'
+	//'three-d-auto-controls',
+	//'three-d-geometric-models',
+	//'three-d-geometric-models-stl'
+]);
 
 
-	/* use the $.fn.circuitboard method to instantiate the circuit-board */
-	$(document).ready(() => {
-		$('#circuitboard').circuitboard({
-			model: getFmaModels(['24tile:60000000'])[0],
-			tileSpacing: 1,
-			tilemapMargin: 0,
-			weightWhenOpen: 8,
-			threeDCanvasElement: $('#three-d-canvas')
-		}).circuitboard('instance').then(function (circuitboard) {
+/* use the $.fn.circuitboard method to instantiate the circuit-board */
+$(document).ready(() => {
 
-			console.info('circuitboard loaded');
+	$('#circuitboard').circuitboard({
+		model: getFmaModels(['24tile:60000000'])[0],
+		tileSpacing: 1,
+		tilemapMargin: 0,
+		weightWhenOpen: 8,
+		threeDCanvasElement: $('#three-d-canvas')
+	}).circuitboard('instance').then(function (circuitboard) {
 
-			window.setThreeDMode = function (mode) {
-				circuitboard.threeDMode = mode;
-			};
+		console.info('circuitboard loaded');
 
-		});
+		window.setThreeDMode = function (mode) {
+			circuitboard.threeDMode = mode;
+		};
+
 	});
-
 
 });
